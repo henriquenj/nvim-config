ce8f192b039546e025b36df76a17c7d4f7cbe31d
Henrique Jung <henriquenj@gmail.com>
2026-06-29 23:39:42 +0200

    feat: add ToggleHardWrap command for 80-column hard wrapping

--- lua/commands.lua
+++ lua/commands.lua
@@ -16,3 +16,22 @@ end
 vim.api.nvim_create_user_command("TrimWhitespace", trim_whitespace, {
   desc = "Delete trailing whitespace in current buffer",
 })
+
+-- ToggleHardWrap
+-- Toggles automatic hard line breaks at 80 columns for the current buffer.
+-- Off by default; turn on when writing prose/markdown you want hard-wrapped.
+local function toggle_hard_wrap()
+  if vim.bo.textwidth == 0 then
+    vim.bo.textwidth = 80
+    vim.opt_local.formatoptions:append("t")
+    vim.notify("Hard wrap ON (textwidth=80)")
+  else
+    vim.bo.textwidth = 0
+    vim.opt_local.formatoptions:remove("t")
+    vim.notify("Hard wrap OFF")
+  end
+end
+
+vim.api.nvim_create_user_command("ToggleHardWrap", toggle_hard_wrap, {
+  desc = "Toggle hard line breaks at 80 columns",
+})
